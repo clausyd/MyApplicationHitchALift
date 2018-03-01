889f707b21ab9030d472ac883db14e6d7489f264
clausyd <clausy1984@hotmial.com>
2018-03-01 20:14:12 +0000

    Working on ListView

---
+++ app/src/main/java/customAdapters/JourneyAdapter.java
@@ -0,0 +1,79 @@
+package customAdapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import ie.wit.screens.R;
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmBaseAdapter;
+import io.realm.RealmResults;
+import models.Journey;
+
+
+/**
+ * Created by clausyd on 28/02/18.
+ */
+//Medium. (2018). Custom Array Adapters made Easy! – Mindorks – Medium. [online] Available at: https://medium.com/mindorks/custom-array-adapters-made-easy-b6c4930560dd [Accessed 28 Feb. 2018].
+public class JourneyAdapter extends RealmBaseAdapter {
+
+    private Context context;
+    private OrderedRealmCollection <Journey> realmCollection;
+
+    public JourneyAdapter(@NonNull Context context, RealmResults<Journey> realmCollection) {
+        super(realmCollection);
+        this.realmCollection = realmCollection;
+        this.context = context;
+    }
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        RowListviewBinding rowListviewBinding;
+//        if(convertView == null){
+//            convertView = LayoutInflater.from(context).inflate(R.layout.journey_row, null);
+//            rowListviewBinding = DataBindingUtil.bind(convertView);
+//            convertView.setTag(rowListviewBinding);
+//        }else{
+//            rowListviewBinding = (RowListviewBinding)convertView.getTag();
+//        }
+//        rowListviewBinding.setPerson(realmCollection.get(position));
+//        return rowListviewBinding.getRoot();
+//    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(context).inflate(R.layout.journey_row,parent,false);
+
+        Journey currentJourney = realmCollection.get(position);
+
+        TextView from = listItem.findViewById(R.id.row_journeyFrom);
+        from.setText(currentJourney.getStartCounty());
+
+        TextView to = (TextView) listItem.findViewById(R.id.row_journeyTo);
+        to.setText(currentJourney.getFinishCounty());
+
+           TextView date = (TextView) listItem.findViewById(R.id.row_journeyDate);
+            date.setText(currentJourney.getDate());
+
+       TextView email = (TextView) listItem.findViewById(R.id.row_email);
+        email.setText(currentJourney.getDate());
+
+        return listItem;
+    }
+
+    @Override
+    public int getCount() {
+        return realmCollection.size();
+    }
+}
+
